747c8998d1f943db6a050fc45660e912f47ccd12
Hikari Tran <quang.tran@elpis.game>
2022-09-29 10:59:07 +0700

    add @service to interceptor

--- src/interceptors/rest/TransformResponseInterceptor.ts
+++ src/interceptors/rest/TransformResponseInterceptor.ts
@@ -1,8 +1,10 @@
 import { Model } from 'mongoose';
 import { Action, Interceptor, InterceptorInterface } from 'routing-controllers';
+import { Service } from 'typedi';
 
 import { convertDocument } from '@Middlewares/graphql/TypegooseMiddleware';
 
+@Service()
 @Interceptor()
 export class TransformResponseInterceptor implements InterceptorInterface {
   intercept(action: Action, result: any) {

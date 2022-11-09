005691c835a56c29a0fc575c713018cccfa7a8c5
Hikari Tran <quang.tran@elpis.game>
2022-11-09 13:02:17 +0700

    update httppaginatedresponse

--- src/libs/types/HttpPaginatedResponse.ts
+++ src/libs/types/HttpPaginatedResponse.ts
@@ -1,44 +1,37 @@
-import { Type } from 'class-transformer';
-import { IsArray, IsNumber, ValidateNested } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export interface ClassType<T = any> {
   new (...args: any[]): T;
 }
-export function HttpPaginatedResponse<T>(TClass: ClassType<T>) {
-  class HttpPaginatedResponseClass {
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(type => TClass)
-    items: T[];
+export class HttpPaginatedResponse<T> {
+  items: Array<T>;
 
-    @IsNumber()
-    total: number;
+  @IsNumber()
+  total: number;
 
-    @IsNumber()
-    size: number;
+  @IsNumber()
+  size: number;
 
-    @IsNumber()
-    page: number;
+  @IsNumber()
+  page: number;
 
-    withItems(items: T[]) {
-      this.items = items;
-      return this;
-    }
-
-    withSize(limit: number) {
-      this.size = limit;
-      return this;
-    }
-
-    withPage(page: number) {
-      this.page = page;
-      return this;
-    }
-
-    withTotal(total: number) {
-      this.total = total;
-      return this;
-    }
+  withItems(items: T[]) {
+    this.items = items;
+    return this;
   }
-  return HttpPaginatedResponseClass;
+
+  withSize(limit: number) {
+    this.size = limit;
+    return this;
+  }
+
+  withPage(page: number) {
+    this.page = page;
+    return this;
+  }
+
+  withTotal(total: number) {
+    this.total = total;
+    return this;
+  }
 }
